cf75c654573682ac5fb72ad4a2873f0d414fbcb6
Ivan Kotov <polyzay@gmail.com>
2024-08-04 18:38:14 +0200

    dashboard is growing

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,10 +1,20 @@
 import { FC } from 'react';
+import Tabs from '../../shared/ui/Tabs/Tabs';
+import Vacancies from '../../features/Vacancies/Vacancies';
+import Cvs from '../../features/Cvs/Cvs';
 
 const Dashboard: FC = () => {
 
   return (
     <div>
       <h1>Dashboard</h1>
+      <Tabs
+        tabs={[
+          { name: 'Vacancies', content: <Vacancies /> },
+          { name: 'CVs', content: <Cvs /> },
+        ]}
+        defaultActiveTab="Vacancies"
+      />
     </div>
   );
 };
