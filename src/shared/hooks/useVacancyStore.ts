81935d42223875ee86ad763b5cc1680d18c4f1b4
Ivan Kotov <polyzay@gmail.com>
2024-09-04 23:28:53 +0200

    vacancyType saver

---
+++ src/shared/hooks/useVacancyStore.ts
@@ -0,0 +1,12 @@
+import create from 'zustand';
+import {VacancyType} from '../types/vacanciesTypes';
+
+interface VacancyStore {
+  vacancyType: VacancyType;
+  setVacancyType: (type: VacancyType) => void;
+}
+
+export const useVacancyStore = create<VacancyStore>((set) => ({
+  vacancyType: 'remote',
+  setVacancyType: (type) => set({ vacancyType: type })
+}))
--- src/shared/ui/Tabs/ui/Tabs.tsx
+++ src/shared/ui/Tabs/ui/Tabs.tsx
@@ -1,16 +1,23 @@
-import { FC, useState } from 'react';
+import { FC, useEffect, useState } from 'react';
 
 import { TabsProps } from '../model/types';
+import { useVacancyStore } from '../../../hooks/useVacancyStore';
 import styles from './Tabs.module.css';
 
 
 const Tabs: FC<TabsProps> = ({ tabs, defaultActiveTab }) => {
   const [activeTab, setActiveTab] = useState(defaultActiveTab || tabs[0].name);
+  const setVacancyType = useVacancyStore((state) => state.setVacancyType);
+
+  useEffect(() => {
+    if (activeTab === 'Удаленка') setVacancyType('remote');
+    else if (activeTab === 'На месте') setVacancyType('onsite');
+  }, [activeTab, setVacancyType]);
 
   return (
     <div className={styles.container}>
       <div className={styles.name}>
-        {tabs.map((tab) => (
+        {tabs.map((tab, ) => (
           <button
             key={tab.name}
             className={tab.name === activeTab ? styles.activeTab : styles.tab}
